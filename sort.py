df1b7474b0eacf4298a458ee03807f69f8a08cdd
Jackson Gunther <jackson.gunther@yahoo.com>
2021-09-20 19:19:21 -0600

    insertion sort working

--- sort.py
+++ sort.py
@@ -3,6 +3,8 @@ import random
 SEED_VALUE = int(100 * random.random())
 lyst = random.sample(range(1000000), k=SEED_VALUE)
 print(lyst)
+
+
 def selection_sort(my_list):
     for i in range(len(my_list)):
         select_min = i
@@ -13,9 +15,19 @@ def selection_sort(my_list):
             my_list[i], my_list[select_min] = my_list[select_min], my_list[i]
     return my_list
 
+
+def insertion_sort(my_list):
+    for i in range(len(my_list)):
+        for j in range(i+1, len(my_list)):
+            if my_list[i] > my_list[j]:
+                my_list[i], my_list[j] = my_list[j], my_list[i]
+                return my_list
+
+
 def main():
-    print(selection_sort(lyst))
+    #print(selection_sort(lyst))
+    print(insertion_sort(lyst))
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     main()
-
